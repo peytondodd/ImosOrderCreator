19a058e41618cf57798d054c074f812ca9d2574e
Peyton Dodd <19616994+peytondodd@users.noreply.github.com>
2018-04-23 23:29:15 -0500

    s

--- check.py
+++ check.py
@@ -14,12 +14,13 @@ REGISTER = ('https://www.letsget.net/(S(jggjirci2tdctkj20ivzf4j5))/Public/Custom
 LOGIN_ID = ('ctl00_BP_Content_lnkLogin')
 
 def register():
-   driver = webdriver.Firefox()
-   driver.get(REGISTER)
+    driver = webdriver.Firefox()
+    driver.get(REGISTER)
 
 def account_create():
-   driver.find_element_by_id('ctl00_BP_Content_uLoginInfo_txtEmailAddress').clear()
-   driver.find_element_by_id('ctl00_BP_Content_uLoginInfo_txtEmailAddress').send_keys(IMOS_CITY) 
+	driver = webdriver.Firefox()
+    driver.find_element_by_id('ctl00_BP_Content_uLoginInfo_txtEmailAddress').clear()
+    driver.find_element_by_id('ctl00_BP_Content_uLoginInfo_txtEmailAddress').send_keys(IMOS_CITY) 
    
 if __name__ == '__main__':
     register()
